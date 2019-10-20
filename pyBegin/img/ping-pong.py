4968f7b957e11d22017914c61808bd108405ed32
vovodstanislava <42979412+vovodstanislava@users.noreply.github.com>
2019-10-20 10:56:16 +0300

    Add files via upload

---
+++ pyBegin/img/ping-pong.py
@@ -0,0 +1,86 @@
+import pygame
+
+pygame.init()
+win = pygame.display.set_mode((500,500))
+
+pygame.display.set_caption("Cubes Game")
+
+walkRight = [pygame.image.load('right_1.png')], [pygame.image.load('right_2.png')], [pygame.image.load('right_3.png')], [pygame.image.load('right_4.png')], [pygame.image.load('right_5.png')],[pygame.image.load('right_6.png')]
+
+walkLeft = [pygame.image.load('left_1.png')], [pygame.image.load('left_2.png')], [pygame.image.load('left_3.png')], [pygame.image.load('left_4.png')], [pygame.image.load('left_5.png')],[pygame.image.load('left_6.png')]
+
+bg = pygame.image.load('bg.jpg')
+playerStand = pygame.image.load('idle.png')
+
+clock = pygame.time.Clock()
+
+x = 50
+y = 425
+widht = 60
+height = 71
+speed = 5
+
+isJump = False
+jumpCount = 10
+
+left = False
+right = False
+animCount = 0
+
+
+def drawWindow():
+    global animCount
+    win.blit(bg, (0,0))
+
+    if animCount + 1 >= 30:
+        animCount = 0
+
+    if left:
+        win.blit(walkLeft[animCount // 5], (x, y))
+        animCount += 1
+    elif right:
+        win.blit(walkRight[animCount // 5], (x, y))
+        animCount += 1
+    else:
+        win.blit(playerStand, (x, y))
+
+    pygame.display.update()
+
+run = True
+while run:
+
+    drawWindow()
+    clock.tick(30)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and x > 5:
+        x -= speed
+
+    elif keys[pygame.K_RIGHT] and x < 500 - widht - 5:
+        x += speed
+
+    else:
+
+        animCount = 0
+    if not(isJump):
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            if jumpCount < 0:
+                y += (jumpCount ** 2) / 2
+            else:
+                 y -= (jumpCount ** 2) / 2
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 10
+
+
+drawWindow()
+
+pygame.quit()
